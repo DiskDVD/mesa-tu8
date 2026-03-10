d218131585db82a6e68f922f1585ee1b03cc46cf
GPT363 <dmitrijnemov13@gmail.com>
2026-03-10 08:59:18 +0300

    Update tu_device.cc

--- src/freedreno/vulkan/tu_device.cc
+++ src/freedreno/vulkan/tu_device.cc
@@ -988,21 +988,21 @@ tu_get_physical_device_properties_1_2(struct tu_physical_device *pdevice,
    p->robustBufferAccessUpdateAfterBind                  = true;
    p->quadDivergentImplicitLod                           = false;
 
-   p->maxUpdateAfterBindDescriptorsInAllPools            = max_descriptor_set_size;
-   p->maxPerStageDescriptorUpdateAfterBindSamplers       = max_descriptor_set_size;
-   p->maxPerStageDescriptorUpdateAfterBindUniformBuffers = max_descriptor_set_size;
-   p->maxPerStageDescriptorUpdateAfterBindStorageBuffers = max_descriptor_set_size;
-   p->maxPerStageDescriptorUpdateAfterBindSampledImages  = max_descriptor_set_size;
-   p->maxPerStageDescriptorUpdateAfterBindStorageImages  = max_descriptor_set_size;
+   p->maxUpdateAfterBindDescriptorsInAllPools            = 500000;
+   p->maxPerStageDescriptorUpdateAfterBindSamplers       = 500000;
+   p->maxPerStageDescriptorUpdateAfterBindUniformBuffers = 500000;
+   p->maxPerStageDescriptorUpdateAfterBindStorageBuffers = 500000;
+   p->maxPerStageDescriptorUpdateAfterBindSampledImages  = 500000;
+   p->maxPerStageDescriptorUpdateAfterBindStorageImages  = 500000;
    p->maxPerStageDescriptorUpdateAfterBindInputAttachments = MAX_RTS;
-   p->maxPerStageUpdateAfterBindResources                = max_descriptor_set_size;
-   p->maxDescriptorSetUpdateAfterBindSamplers            = max_descriptor_set_size;
-   p->maxDescriptorSetUpdateAfterBindUniformBuffers      = max_descriptor_set_size;
+   p->maxPerStageUpdateAfterBindResources                = 500000;
+   p->maxDescriptorSetUpdateAfterBindSamplers            = 500000;
+   p->maxDescriptorSetUpdateAfterBindUniformBuffers      = 500000;
    p->maxDescriptorSetUpdateAfterBindUniformBuffersDynamic = MAX_DYNAMIC_UNIFORM_BUFFERS;
-   p->maxDescriptorSetUpdateAfterBindStorageBuffers      = max_descriptor_set_size;
+   p->maxDescriptorSetUpdateAfterBindStorageBuffers      = 500000;
    p->maxDescriptorSetUpdateAfterBindStorageBuffersDynamic = MAX_DYNAMIC_STORAGE_BUFFERS;
-   p->maxDescriptorSetUpdateAfterBindSampledImages       = max_descriptor_set_size;
-   p->maxDescriptorSetUpdateAfterBindStorageImages       = max_descriptor_set_size;
+   p->maxDescriptorSetUpdateAfterBindSampledImages       = 500000;
+   p->maxDescriptorSetUpdateAfterBindStorageImages       = 500000;
    p->maxDescriptorSetUpdateAfterBindInputAttachments    = MAX_RTS;
 
    p->supportedDepthResolveModes    =
