d13f669ec724cac7e676980adfe350cfd1c80f47
DVD <dmitrijnemov13@gmail.com>
2026-03-12 00:47:18 +0300

    Update freedreno_devices.py

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1634,7 +1634,7 @@ a8xx_810 = GPUProps(
         sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
         sysmem_per_ccu_color_cache_size = 32 * 1024,
         sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
-        sysmem_per_ccu_depth_cache_size = 64 * 1024,
+        sysmem_per_ccu_depth_cache_size = 32 * 1024,
         
         # GMEM VPC буферы - СТАБИЛЬНЫЕ ЗНАЧЕНИЯ
         gmem_vpc_attr_buf_size = 49152,         # 48KB - стабильно
