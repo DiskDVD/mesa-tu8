91840099a53310741af225d7b9b5412c94ca3a07
DVD <dmitrijnemov13@gmail.com>
2026-03-14 10:40:35 +0300

    Update freedreno_devices.py

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1637,15 +1637,15 @@ a8xx_810 = GPUProps(
         sysmem_per_ccu_depth_cache_size = 32 * 1024,
         
         # GMEM VPC буферы - СТАБИЛЬНЫЕ ЗНАЧЕНИЯ
-        gmem_vpc_attr_buf_size = 16384,         # 48KB - стабильно
-        gmem_vpc_pos_buf_size = 4096,          # 24KB - стабильно
-        gmem_vpc_bv_pos_buf_size = 4096,       # 32KB - стабильно
+        gmem_vpc_attr_buf_size = 49152,         # 48KB - стабильно
+        gmem_vpc_pos_buf_size = 24576,          # 24KB - стабильно
+        gmem_vpc_bv_pos_buf_size = 32768,       # 32KB - стабильно
         
         # GMEM кэши - без изменений
         gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-        gmem_per_ccu_color_cache_size = 4096,
+        gmem_per_ccu_color_cache_size = 8 * 1024,
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-        gmem_per_ccu_depth_cache_size = 8192,
+        gmem_per_ccu_depth_cache_size = 32 * 1024,
         
         gmem_size = 512 * 1024,  # 512kb GMEM, 4MB????       
         # A810 не поддерживает ray tracing
