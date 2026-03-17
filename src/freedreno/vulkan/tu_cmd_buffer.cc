cb7e5ac0448817b0e7bb4918b8aadfbfeaa7b537
DVD <dmitrijnemov13@gmail.com>
2026-03-17 10:52:55 +0300

    Update tu_cmd_buffer.cc
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/vulkan/tu_cmd_buffer.cc
+++ src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -1076,22 +1076,22 @@ tu6_emit_blit_scissor(struct tu_cmd_buffer *cmd, struct tu_cs *cs,
    uint32_t y1 = render_area->offset.y;
    uint32_t x2 = x1 + render_area->extent.width - 1;
    uint32_t y2 = y1 + render_area->extent.height - 1;
-      /* ===== ФИКС ДЛЯ ADRENO 810 ===== */
+   
+   /* ===== ФИКС ВЫРАВНИВАНИЯ ДЛЯ ADRENO 810 ===== */
    if (cmd->device->physical_device->dev_id.gpu_id == 810) {
-      /* Принудительное выравнивание на границы 32x16 */
+       /* Принудительное выравнивание 32x16 для A810 */
       x1 = x1 & ~31;
       y1 = y1 & ~15;
       x2 = ((x2 + 32) & ~31) - 1;
       y2 = ((y2 + 16) & ~15) - 1;
    } else if (align) {
-      /* Стандартное выравнивание */
+      /* Стандартное выравнивание для других GPU */
       x1 = x1 & ~(phys_dev->info->gmem_align_w - 1);
       y1 = y1 & ~(phys_dev->info->gmem_align_h - 1);
       x2 = ALIGN_POT(x2 + 1, phys_dev->info->gmem_align_w) - 1;
       y2 = ALIGN_POT(y2 + 1, phys_dev->info->gmem_align_h) - 1;
    }
-   /* ===== КОНЕЦ ФИКСА ===== */
-   
+   /* ===== КОНЕЦ ФИКСА ===== */  
 
    if (align) {
       x1 = x1 & ~(phys_dev->info->gmem_align_w - 1);
