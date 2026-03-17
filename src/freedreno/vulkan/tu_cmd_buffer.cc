016bc0dcdf4bbf6ec521df23d35dd08bd42e7d20
DVD <dmitrijnemov13@gmail.com>
2026-03-17 10:45:25 +0300

    Update tu_cmd_buffer.cc
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/vulkan/tu_cmd_buffer.cc
+++ src/freedreno/vulkan/tu_cmd_buffer.cc
@@ -7265,17 +7265,7 @@ tu_CmdBeginRenderPass2(VkCommandBuffer commandBuffer,
       }
    }
 
-tu_choose_gmem_layout(cmd);
-/* ===== ФИКС ДЛЯ ADRENO 810 ===== */
-if (cmd->device->physical_device->dev_id.gpu_id == 810) {
-   /* Принудительно используем размер тайла 192x192 для всех операций */
-   cmd->state.tile_width = 192;
-   cmd->state.tile_height = 192;
-   
-   /* Добавляем отладочную информацию */
-   mesa_logi("A810: Using forced tile size %ux%u", 192, 192);
-}
-/* ===== КОНЕЦ ФИКСА ===== */
+
 
 
 /* Note: because this is external, any flushes will happen before draw_cs
@@ -7286,7 +7276,19 @@ tu_subpass_barrier(cmd, &pass->subpasses[0].start_barrier, true);
    cmd->state.renderpass_cache.pending_flush_bits =
       cmd->state.cache.pending_flush_bits;
    cmd->state.renderpass_cache.flush_bits = 0;
+   
+tu_choose_gmem_layout(cmd);
 
+/* ===== ФИКС ВЫРАВНИВАНИЯ ДЛЯ ADRENO 810 ===== */
+if (cmd->device->physical_device->dev_id.gpu_id == 810) {
+   /* Принудительно устанавливаем выравнивание в физическом устройстве */
+   struct tu_physical_device *phys_dev = cmd->device->physical_device;
+   phys_dev->info->gmem_align_w = 32;
+   phys_dev->info->gmem_align_h = 16;
+   
+   mesa_logi("A810: Forced alignment in phys_dev to %ux%u", 32, 16);
+}
+/* ===== КОНЕЦ ФИКСА ===== */
    if (pass->subpasses[0].feedback_invalidate) {
       cmd->state.renderpass_cache.flush_bits |=
          TU_CMD_FLAG_CACHE_INVALIDATE | TU_CMD_FLAG_BLIT_CACHE_CLEAN |
