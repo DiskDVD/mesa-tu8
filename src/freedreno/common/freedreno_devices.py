6f57ee3d26f5aa1a8e3e3ebc1ab7ff0ef6e64c24
DVD <dmitrijnemov13@gmail.com>
2026-03-18 00:06:17 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1666,7 +1666,7 @@ add_gpus([
         num_ccu = 2,
         num_slices = 1,
         tile_align_w = 32,
-        tile_align_h = 12,
+        tile_align_h = 16,
         tile_max_w = 192,
         tile_max_h = 192,
         num_vsc_pipes = 32,
