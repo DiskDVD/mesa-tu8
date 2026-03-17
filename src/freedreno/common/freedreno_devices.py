2683fbefa102f4353ec3061ced3bac5f17dcfd8d
DVD <dmitrijnemov13@gmail.com>
2026-03-17 18:08:12 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1643,7 +1643,7 @@ a8xx_810 = GPUProps(
         
         # GMEM кэши - без изменений
         gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-        gmem_per_ccu_color_cache_size = 32 * 1024,
+        gmem_per_ccu_color_cache_size = 24 * 1024,
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
         gmem_per_ccu_depth_cache_size = 64 * 1024,
         
@@ -1667,8 +1667,8 @@ add_gpus([
         num_slices = 1,
         tile_align_w = 16,
         tile_align_h = 16,
-        tile_max_w = 192,
-        tile_max_h =  192,
+        tile_max_w = 64,
+        tile_max_h =  64,
         num_vsc_pipes = 32,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
