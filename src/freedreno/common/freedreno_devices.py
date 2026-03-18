e9a86ba469fd0d7bc3043d332a586105dd1eb67f
DVD <dmitrijnemov13@gmail.com>
2026-03-18 17:44:57 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1637,13 +1637,13 @@ a8xx_810 = GPUProps(
         sysmem_per_ccu_depth_cache_size = 32 * 1024,
         
         # GMEM VPC буферы - СТАБИЛЬНЫЕ ЗНАЧЕНИЯ
-        gmem_vpc_attr_buf_size = 49152,         # 48KB - стабильно
-        gmem_vpc_pos_buf_size = 24576,          # 24KB - стабильно
-        gmem_vpc_bv_pos_buf_size = 32768,       # 32KB - стабильно
+        gmem_vpc_attr_buf_size = 36864,         
+        gmem_vpc_pos_buf_size = 18432,          
+        gmem_vpc_bv_pos_buf_size = 24576, 
         
         # GMEM кэши - без изменений
         gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-        gmem_per_ccu_color_cache_size = 16 * 1024,
+        gmem_per_ccu_color_cache_size = 26 * 1024,
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
         gmem_per_ccu_depth_cache_size = 64 * 1024,
         
@@ -1665,8 +1665,8 @@ add_gpus([
         [a7xx_base, a7xx_gen3, a8xx_base, a8xx_810],
         num_ccu = 2,
         num_slices = 1,
-        tile_align_w = 32,
-        tile_align_h = 16,
+        tile_align_w = 64,
+        tile_align_h = 32,
         tile_max_w = 16384,
         tile_max_h = 16384,
         num_vsc_pipes = 32,
