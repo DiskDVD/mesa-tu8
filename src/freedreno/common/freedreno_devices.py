ba6bbe108fb945775d62685a19d156bb77ac4968
DVD <dmitrijnemov13@gmail.com>
2026-03-19 10:13:55 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1647,7 +1647,7 @@ a8xx_810 = GPUProps(
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
         gmem_per_ccu_depth_cache_size = 64 * 1024,
         
-        gmem_size = 512 * 1024,  # 512kb GMEM, 4MB????       
+        gmem_size = 576 * 1024,  # 576kb GMEM, 4MB????       
         # A810 не поддерживает ray tracing
         has_ray_intersection = False,
         has_sw_fuse = False,
