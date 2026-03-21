94af12c92db4a60356c9e7f4817d757dbbad411e
DVD <dmitrijnemov13@gmail.com>
2026-03-21 13:16:08 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1605,20 +1605,27 @@ a8xx_825 = GPUProps(
 )
 
 a8xx_829 = GPUProps(
-        sysmem_vpc_attr_buf_size = 131072,
-        sysmem_vpc_pos_buf_size = 65536,
-        sysmem_vpc_bv_pos_buf_size = 32768,
+        sysmem_vpc_attr_buf_size = 65536,
+        sysmem_vpc_pos_buf_size = 32768,
+        sysmem_vpc_bv_pos_buf_size = 16384,
         sysmem_ccu_color_cache_fraction = CCUColorCacheFraction.FULL.value,
-        sysmem_per_ccu_color_cache_size = 128 * 1024,
+        sysmem_per_ccu_color_cache_size = 32 * 1024,
         sysmem_ccu_depth_cache_fraction = CCUColorCacheFraction.THREE_QUARTER.value,
-        sysmem_per_ccu_depth_cache_size = 96 * 1024,
-        gmem_vpc_attr_buf_size = 49152,
-        gmem_vpc_pos_buf_size = 24576,
+        sysmem_per_ccu_depth_cache_size = 32 * 1024,
+        gmem_vpc_attr_buf_size = 65536,
+        gmem_vpc_pos_buf_size = 32768,
         gmem_vpc_bv_pos_buf_size = 32768,
         gmem_ccu_color_cache_fraction = CCUColorCacheFraction.EIGHTH.value,
-        gmem_per_ccu_color_cache_size = 16 * 1024,
+        gmem_per_ccu_color_cache_size = 256 * 1024,
         gmem_ccu_depth_cache_fraction = CCUColorCacheFraction.FULL.value,
-        gmem_per_ccu_depth_cache_size = 127 * 1024,
+        gmem_per_ccu_depth_cache_size = 512 * 1024,
+        gmem_size = 2 * 1024 * 1024,
+
+        has_ray_intersection = True,
+        has_sw_fuse = False,
+        disable_gmem = False,
+        has_coherent_ubws_flag_caches = True,
+        has_fs_tex_prefetch = True,
 )
 
 # Оптимизированные параметры для Adreno 810
@@ -1716,8 +1723,8 @@ add_gpus([
         num_slices = 2,
         tile_align_w = 64,
         tile_align_h = 32,
-        tile_max_w = 16384,
-        tile_max_h = 16384,
+        tile_max_w = 256,
+        tile_max_h = 256,
         num_vsc_pipes = 32,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
