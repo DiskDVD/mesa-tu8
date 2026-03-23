b6eebbe7821552ce0c9a13272ba8eb504b58cbe3
DVD <dmitrijnemov13@gmail.com>
2026-03-23 20:51:58 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1662,10 +1662,7 @@ a8xx_810 = GPUProps(
         has_coherent_ubwc_flag_caches = True,
         has_fs_tex_prefetch = False,
         has_salu_int_narrowing_quirk = True,
-        enable_fsr = True,
-        fsr_render_scale = 0.5,
-        fsr_sharpness = 0.2,
-        force_sysmem = True,
+        
 )
 
 
