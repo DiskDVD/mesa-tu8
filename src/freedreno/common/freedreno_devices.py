56b8b63aa00a78bcfb771ab8c45b76f5ddb53ad0
DVD <dmitrijnemov13@gmail.com>
2026-03-24 21:20:57 +0300

    Update freedreno_devices.py
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1678,7 +1678,7 @@ add_gpus([
         tile_align_h = 16,
         tile_max_w = 256,
         tile_max_h = 256,
-        num_vsc_pipes = 32,
+        num_vsc_pipes = 16,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
         fibers_per_sp = 128 * 2 * 16,
