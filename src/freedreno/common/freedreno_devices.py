df0fb7cb39da4f67a59e05dc8c31f4eee48ad152
DVD <dmitrijnemov13@gmail.com>
2026-04-13 20:45:43 +0300

    Nilai berubah karena saya salah memasukkan nilai secara tidak sengaja.
    A829
    Tittle_H = 16384
    
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/common/freedreno_devices.py
+++ src/freedreno/common/freedreno_devices.py
@@ -1610,7 +1610,7 @@ add_gpus([
         tile_align_w = 96,
         tile_align_h = 32,
         tile_max_w = 16384,
-        tile_max_h = 16382,
+        tile_max_h = 16384,
         num_vsc_pipes = 32,
         cs_shared_mem_size = 32 * 1024,
         wave_granularity = 2,
