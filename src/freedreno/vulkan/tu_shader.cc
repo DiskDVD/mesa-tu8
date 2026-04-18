70d6262302c06390675e01940ca1e131f4793497
DVD <dmitrijnemov13@gmail.com>
2026-04-18 13:53:40 +0300

    Fix error compiler
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/vulkan/tu_shader.cc
+++ src/freedreno/vulkan/tu_shader.cc
@@ -2188,31 +2188,9 @@ tu6_emit_fs_inputs(struct tu_cs *cs, const struct ir3_shader_variant *fs)
          if (VALIDREG(sysval))
             sysval_regs += 2;
       }
-
 sysval_regs = MIN2(sysval_regs, profile.sysval_regs_cap);
-bool defer_wave_alloc_dis = true;
+      bool defer_wave_alloc_dis = profile.defer_wave_alloc_dis;
 
-   if (CHIP >= A8XX) {
-      uint32_t chip = dev->physical_device->chip_id;
-
-      switch (chip) {
-      case 0x44010000:          /* Adreno 810 */
-      case 0xffff44010000:
-      case 0x44030A20:          /* Adreno 829 */
-         defer_wave_alloc_dis = true;
-         break;
-
-      case 0x44050001:          /* Adreno 830 */
-      case 0xffff44050000:
-      case 0xffff44050A31:      /* Adreno 840 */
-         defer_wave_alloc_dis = false;
-         break;
-
-      default:
-         defer_wave_alloc_dis = false;
-         break;
-      }
-   }
 
       tu_cs_emit_regs(cs, SP_PS_CNTL_1(CHIP,
          .sysval_regs_count = sysval_regs,
