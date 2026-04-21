413cf79a36223c1eee20e1fa7d1a4f830bd74747
DVD <dmitrijnemov13@gmail.com>
2026-04-21 23:50:33 +0300

    Rollback files
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/vulkan/tu_shader.h
+++ src/freedreno/vulkan/tu_shader.h
@@ -132,8 +132,6 @@ struct tu_shader_key {
    bool custom_resolve;
    bool emulate_alpha_to_coverage;
    enum ir3_wavesize_option api_wavesize, real_wavesize;
-   bool optimize_for_810_cpu;
-   bool optimize_for_829_ram;
 };
 
 /* Information needed for tu_shader creation that is gathered during NIR
