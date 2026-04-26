1a6e86e103fdaa0adc848fc0d11d89d587b866fd
DVD <dmitrijnemov13@gmail.com>
2026-04-26 16:17:26 +0300

    Rollback to stock
    Signed-off-by: DVD  <dmitrijnemov13@gmail.com>

--- src/freedreno/ir3/ir3_compiler.c
+++ src/freedreno/ir3/ir3_compiler.c
@@ -325,8 +325,7 @@ ir3_compiler_create(struct fd_device *dev, const struct fd_dev_id *dev_id,
        *
        * TODO: is this true on earlier gen's?
        */
-      compiler->max_const_compute =
-         compiler->gen >= 8 ? 1024 : (compiler->gen >= 7 ? 512 : 256); /* A8xx Optimization */
+      compiler->max_const_compute = compiler->gen >= 7 ? 512 : 256;
 
       if (dev_info->props.is_a702) {
          /* No GS/tess, 128 per stage otherwise: */
@@ -383,7 +382,7 @@ ir3_compiler_create(struct fd_device *dev, const struct fd_dev_id *dev_id,
       compiler->compute_lb_size = dev_info->compute_lb_size;
    } else {
       compiler->compute_lb_size =
-         compiler->max_const_compute * 16 /* bytes/vec4, keep in sync with max_const_compute (A8xx Optimization) */ *
+         compiler->max_const_compute * 16 /* bytes/vec4 */ *
          compiler->info->wave_granularity + compiler->info->cs_shared_mem_size;
    }
 
